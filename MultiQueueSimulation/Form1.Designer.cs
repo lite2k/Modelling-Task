8e904028a618716575c723a938712d24b91d8d58
Youssef El Sayed <ysayed38@gmail.com>
2022-03-15 18:35:21 +0200

    Input Gui finished

--- MultiQueueSimulation/Form1.Designer.cs
+++ MultiQueueSimulation/Form1.Designer.cs
@@ -28,21 +28,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
             this.Browse_Button = new System.Windows.Forms.Button();
             this.NS = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.SC = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.SN = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.SM = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Random_radioButton = new System.Windows.Forms.RadioButton();
+            this.HP_radioButton = new System.Windows.Forms.RadioButton();
+            this.StoppingCriteria_group = new System.Windows.Forms.GroupBox();
+            this.EndTime_radioButton = new System.Windows.Forms.RadioButton();
+            this.Number_radioButton = new System.Windows.Forms.RadioButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            this.StoppingCriteria_group.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // Browse_Button
             // 
-            this.Browse_Button.Location = new System.Drawing.Point(455, 12);
+            this.Browse_Button.Location = new System.Drawing.Point(475, 28);
             this.Browse_Button.Name = "Browse_Button";
             this.Browse_Button.Size = new System.Drawing.Size(75, 23);
             this.Browse_Button.TabIndex = 0;
@@ -52,7 +59,7 @@
             // 
             // NS
             // 
-            this.NS.Location = new System.Drawing.Point(135, 125);
+            this.NS.Location = new System.Drawing.Point(131, 66);
             this.NS.Name = "NS";
             this.NS.Size = new System.Drawing.Size(100, 22);
             this.NS.TabIndex = 1;
@@ -60,32 +67,16 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 125);
+            this.label1.Location = new System.Drawing.Point(9, 66);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(116, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "Number of service";
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 248);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(99, 16);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Stopping critiria";
-            // 
-            // SC
-            // 
-            this.SC.Location = new System.Drawing.Point(135, 242);
-            this.SC.Name = "SC";
-            this.SC.Size = new System.Drawing.Size(100, 22);
-            this.SC.TabIndex = 3;
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 185);
+            this.label3.Location = new System.Drawing.Point(12, 131);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(109, 16);
             this.label3.TabIndex = 6;
@@ -93,52 +84,123 @@
             // 
             // SN
             // 
-            this.SN.Location = new System.Drawing.Point(135, 185);
+            this.SN.Location = new System.Drawing.Point(131, 125);
             this.SN.Name = "SN";
             this.SN.Size = new System.Drawing.Size(100, 22);
             this.SN.TabIndex = 5;
             // 
-            // label4
+            // groupBox1
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 303);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(111, 16);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Selection method";
+            this.groupBox1.Controls.Add(this.Random_radioButton);
+            this.groupBox1.Controls.Add(this.HP_radioButton);
+            this.groupBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.groupBox1.Location = new System.Drawing.Point(12, 170);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "SelectionMethod";
             // 
-            // SM
+            // Random_radioButton
             // 
-            this.SM.Location = new System.Drawing.Point(134, 303);
-            this.SM.Name = "SM";
-            this.SM.Size = new System.Drawing.Size(100, 22);
-            this.SM.TabIndex = 7;
+            this.Random_radioButton.AutoSize = true;
+            this.Random_radioButton.Location = new System.Drawing.Point(19, 49);
+            this.Random_radioButton.Name = "Random_radioButton";
+            this.Random_radioButton.Size = new System.Drawing.Size(80, 20);
+            this.Random_radioButton.TabIndex = 1;
+            this.Random_radioButton.TabStop = true;
+            this.Random_radioButton.Text = "Random";
+            this.Random_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // HP_radioButton
+            // 
+            this.HP_radioButton.AutoSize = true;
+            this.HP_radioButton.Location = new System.Drawing.Point(19, 22);
+            this.HP_radioButton.Name = "HP_radioButton";
+            this.HP_radioButton.Size = new System.Drawing.Size(118, 20);
+            this.HP_radioButton.TabIndex = 0;
+            this.HP_radioButton.TabStop = true;
+            this.HP_radioButton.Text = "Highest Priority";
+            this.HP_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // StoppingCriteria_group
+            // 
+            this.StoppingCriteria_group.Controls.Add(this.EndTime_radioButton);
+            this.StoppingCriteria_group.Controls.Add(this.Number_radioButton);
+            this.StoppingCriteria_group.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.StoppingCriteria_group.Location = new System.Drawing.Point(15, 295);
+            this.StoppingCriteria_group.Name = "StoppingCriteria_group";
+            this.StoppingCriteria_group.Size = new System.Drawing.Size(200, 100);
+            this.StoppingCriteria_group.TabIndex = 11;
+            this.StoppingCriteria_group.TabStop = false;
+            this.StoppingCriteria_group.Text = "Stoppin Criteria";
+            // 
+            // EndTime_radioButton
+            // 
+            this.EndTime_radioButton.AutoSize = true;
+            this.EndTime_radioButton.Location = new System.Drawing.Point(19, 49);
+            this.EndTime_radioButton.Name = "EndTime_radioButton";
+            this.EndTime_radioButton.Size = new System.Drawing.Size(86, 20);
+            this.EndTime_radioButton.TabIndex = 1;
+            this.EndTime_radioButton.TabStop = true;
+            this.EndTime_radioButton.Text = "End Time";
+            this.EndTime_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // Number_radioButton
+            // 
+            this.Number_radioButton.AutoSize = true;
+            this.Number_radioButton.Location = new System.Drawing.Point(19, 22);
+            this.Number_radioButton.Name = "Number_radioButton";
+            this.Number_radioButton.Size = new System.Drawing.Size(155, 20);
+            this.Number_radioButton.TabIndex = 0;
+            this.Number_radioButton.TabStop = true;
+            this.Number_radioButton.Text = "Number of customers";
+            this.Number_radioButton.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(272, 77);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(443, 417);
+            this.dataGridView1.TabIndex = 12;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(721, 77);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(469, 417);
+            this.dataGridView2.TabIndex = 13;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-
-            this.ClientSize = new System.Drawing.Size(282, 253);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-
-            this.ClientSize = new System.Drawing.Size(1021, 493);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.SM);
+            this.ClientSize = new System.Drawing.Size(1202, 562);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.StoppingCriteria_group);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.SN);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.SC);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.NS);
             this.Controls.Add(this.Browse_Button);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.StoppingCriteria_group.ResumeLayout(false);
+            this.StoppingCriteria_group.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
-
 
         }
 
@@ -148,13 +210,16 @@
         private System.Windows.Forms.Button Browse_Button;
         private System.Windows.Forms.TextBox NS;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox SC;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox SN;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox SM;
-
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton Random_radioButton;
+        private System.Windows.Forms.RadioButton HP_radioButton;
+        private System.Windows.Forms.GroupBox StoppingCriteria_group;
+        private System.Windows.Forms.RadioButton EndTime_radioButton;
+        private System.Windows.Forms.RadioButton Number_radioButton;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
 
--- MultiQueueSimulation/Form1.cs
+++ MultiQueueSimulation/Form1.cs
@@ -19,9 +19,11 @@ namespace MultiQueueSimulation
         public Form1()
         {
             InitializeComponent();
+            
         }
         private void Browse_Button_Click(object sender, EventArgs e)
         {
+            SimulationSystem simulationSystem = new SimulationSystem();
             int size = -1;
             string text = "";
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
@@ -39,12 +41,104 @@ namespace MultiQueueSimulation
                     Console.WriteLine("File not found");
                 }
                 text = Regex.Replace(text, @"^\s+$[\r\n]*", string.Empty, RegexOptions.Multiline);
+                text = text.Replace("\r", "");
                 string[] elements = text.Split('\n');
-
+                //Number of servers
                 NS.Text = elements[1];
+                simulationSystem.NumberOfServers = int.Parse(elements[1]);
+                //Stopping number
                 SN.Text = elements[3];
-                SC.Text = elements[5];
-                SM.Text = elements[7];
+                simulationSystem.StoppingNumber = int.Parse(elements[3]);
+                //Stoping method
+                string stop = elements[5];
+                //selection method
+                string selection =  elements[7];
+
+                if (selection == "1")
+                {
+                    HP_radioButton.Checked = true;
+                    Random_radioButton.Checked = false;
+                    simulationSystem.SelectionMethod = Enums.SelectionMethod.HighestPriority;
+                }
+                else
+                {
+                    Random_radioButton.Checked = true;
+                    HP_radioButton.Checked = false;
+                    simulationSystem.SelectionMethod = Enums.SelectionMethod.Random;
+                }
+
+                if (stop == "1")
+                {
+                    Number_radioButton.Checked = true;
+                    EndTime_radioButton.Checked = false;
+                    simulationSystem.StoppingCriteria = Enums.StoppingCriteria.NumberOfCustomers;
+                }
+                else
+                {
+                    Number_radioButton.Checked = true;
+                    EndTime_radioButton.Checked = false;
+                    simulationSystem.StoppingCriteria = Enums.StoppingCriteria.SimulationEndTime;
+                }
+                //Interarrival Time
+                int index = 9;
+                for (; !elements[index].Contains("Service"); index++)
+                {
+                    string[] line = elements[index].Split(',');
+                    TimeDistribution t = new TimeDistribution();
+                    t.Time = int.Parse(line[0]);
+                    t.Probability = decimal.Parse(line[1]);
+                    simulationSystem.InterarrivalDistribution.Add(t);
+                }
+
+                dataGridView1.ColumnCount = 2;
+                dataGridView1.Columns[0].Name = "Interarrival Time";
+                dataGridView1.Columns[1].Name = "Probability";
+
+                for (int i = 0; i < simulationSystem.InterarrivalDistribution.Count; i++)
+                {
+                        string[] row = new string[] { simulationSystem.InterarrivalDistribution[i].Time.ToString(),
+                                                      simulationSystem.InterarrivalDistribution[i].Probability.ToString()};
+                        dataGridView1.Rows.Add(row);
+                }
+
+                //server
+                int serverIndex=0;
+                while (index < elements.Length)
+                {
+                    
+                    Server server = new Server();
+                    for (index = index + 1; index < elements.Length; index++)
+                    {
+                        if (elements[index].Contains("Service"))
+                            break;
+
+                        string[] line = elements[index].Split(',');
+                        server.ID = serverIndex;
+                        TimeDistribution t = new TimeDistribution();
+                        t.Time = int.Parse(line[0]);
+                        t.Probability = decimal.Parse(line[1]);
+                        server.TimeDistribution.Add(t);
+                    }
+                        serverIndex++;
+                        simulationSystem.Servers.Add(server);
+                }
+                dataGridView2.ColumnCount = 3;
+                dataGridView2.Columns[0].Name = "Server Index";
+                dataGridView2.Columns[1].Name = "Service Time";
+                dataGridView2.Columns[2].Name = "Probability";
+
+                for (int i = 0;i<simulationSystem.Servers.Count;i++)
+                {
+                    for (int j = 0; j < simulationSystem.Servers[i].TimeDistribution.Count; j++)
+                    {
+                        string[] row = new string[] { simulationSystem.Servers[i].ID.ToString(),
+                                                      simulationSystem.Servers[i].TimeDistribution[j].Time.ToString(),
+                                                      simulationSystem.Servers[i].TimeDistribution[j].Probability.ToString()};
+                        dataGridView2.Rows.Add(row);
+                    }
+                }
+
+
 
             }
 
